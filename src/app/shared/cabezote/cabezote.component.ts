767edc3be8d9fffb534477380fd55fe4fd256bc0
Cristian Reveco <crirev@gmail.com>
2020-11-22 16:20:39 -0300

    dsfsdfsdf

---
+++ src/app/shared/cabezote/cabezote.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cabezote',
+  templateUrl: './cabezote.component.html',
+  styleUrls: ['./cabezote.component.less']
+})
+export class CabezoteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,15 +3,18 @@ import { CommonModule } from '@angular/common';
 import { FotterComponent } from './fotter/fotter.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { CabezoteComponent } from './cabezote/cabezote.component';
 
 const component = [
   FotterComponent,
   NavbarComponent,
-  CarouselComponent
+  CarouselComponent,
+  CabezoteComponent
 ]
 @NgModule({
   declarations: [
     ...component,
+    CabezoteComponent,
   ],
   imports: [
     CommonModule
