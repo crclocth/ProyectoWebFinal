0fd00ae8f6fed518b61c4e540669961bf160785f
Cristian Reveco <crirev@gmail.com>
2020-11-23 10:11:51 -0300

    dsadad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
+  { path: '', redirectTo:'/visitor',pathMatch:'full'},
   { path: 'visitor', loadChildren: () => import('./visitor/visitor.module').then (VisitorModule => VisitorModule.VisitorModule)  },
   { path: 'user', loadChildren: () => import('./user/user.module').then (UserModule => UserModule.UserModule)  },
   { path: 'admin', loadChildren: () => import('./admin/admin.module').then (AdminModule => AdminModule.AdminModule)}
