6ddca143cdf73cc3d3c6da3806cd92758a680e02
Cristian Reveco <crirev@gmail.com>
2020-11-28 12:10:40 -0300

    sdasadsad

--- src/app/visitor/screens/login/login.component.ts
+++ src/app/visitor/screens/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  checkoutForm: FormGroup;
+  mensaje:string="";
+  isDivVisible = false;
+
+
+
+  constructor() {
+    this.checkoutForm = this.createFormGroup();
+  }
+
 
   ngOnInit(): void {
   }
+
+
+
+
+  createFormGroup() {
+    return new FormGroup({
+      usuario: new FormControl('', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$")]),
+      password: new FormControl('',[Validators.required]),
+     
+
+    });
+  }
+
+  onSubmit(){
+    
+  }
+  get usuario() { return this.checkoutForm.get('usuario')?.value; }
+  get password() { return this.checkoutForm.get('password')?.value; }
+ 
+
+
+  // GUARDAR DATOS EN ARRAY
+/*
+  public async postUsuario() { 
+    let datosFormulario = {
+
+      nombre: this.checkoutForm.get('nombre')?.value,
+      usuario: this.checkoutForm.get('usuario')?.value, 
+      password: this.checkoutForm.get('password')?.value, 
+      passwordtwo: this.checkoutForm.get('passwordtwo')?.value,
+
+    };
+    console.log(datosFormulario);
+    try {
+      this.mensaje="Se guardaron los datos.";
+      this.isDivVisible=true;
+      await this.visitorProviderService.postUser(datosFormulario).toPromise();
+    }
+    catch (error) {
+      alert("Error al añadir el usuario");
+    } 
+  }*/
 
 }
--- src/app/visitor/screens/sugerencias/sugerencias.component.ts
+++ src/app/visitor/screens/sugerencias/sugerencias.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-sugerencias',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SugerenciasComponent implements OnInit {
 
-  constructor() { }
+  checkoutForm2: FormGroup;
+  mensaje:string="";
+  isDivVisible = false;
+
+  constructor() {
+    this.checkoutForm2 = this.createFormGroup();
+   }
 
   ngOnInit(): void {
   }
+  createFormGroup() {
+    return new FormGroup({
+      usuario2: new FormControl('', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$")]),
+      asunto: new FormControl('', [Validators.required, ]),
+      tex: new FormControl('', [Validators.required, ]),
+
+    });
+  }
+    onSubmit(){
+      this.mensaje="Sugerencia Enviada.";
+      this.isDivVisible=true;
+    }
+
+    get usuario2() { return this.checkoutForm2.get('usuario2')?.value; }
+    get asunto() { return this.checkoutForm2.get('asunto')?.value; }
+    get tex() { return this.checkoutForm2.get('tex')?.value}
 
 }
