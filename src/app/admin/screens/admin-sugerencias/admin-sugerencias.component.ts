449bfda3a8fea8bb18ad92d41bb09153da47eb8f
Cristian Reveco <crirev@gmail.com>
2020-11-28 15:30:33 -0300

    saadadsasd

--- src/app/admin/screens/admin-sugerencias/admin-sugerencias.component.ts
+++ src/app/admin/screens/admin-sugerencias/admin-sugerencias.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-admin-sugerencias',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminSugerenciasComponent implements OnInit {
 
-  constructor() { }
+  checkoutForm2: FormGroup;
+  mensaje:string="";
+  isDivVisible = false;
+
+  constructor() {
+    this.checkoutForm2 = this.createFormGroup();
+   }
 
   ngOnInit(): void {
   }
+  createFormGroup() {
+    return new FormGroup({
+      usuario2: new FormControl('', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$")]),
+      asunto: new FormControl('', [Validators.required, ]),
+      tex: new FormControl('', [Validators.required, ]),
 
+    });
+  }
+    onSubmit(){
+      this.mensaje="Sugerencia Enviada.";
+      this.isDivVisible=true;
+    }
+
+    get usuario2() { return this.checkoutForm2.get('usuario2')?.value; }
+    get asunto() { return this.checkoutForm2.get('asunto')?.value; }
+    get tex() { return this.checkoutForm2.get('tex')?.value}
 }
